b009c8d52c5bbff40de6380f8525116959e0bf9b
Reshul <53306036+resel143@users.noreply.github.com>
2021-04-18 15:09:30 +0530

    Create gettingstartedwithNPM.js

---
+++ gettingstartedwithNPM.js
@@ -0,0 +1,6 @@
+const chalk = require('chalk');
+
+console.log(chalk.yellow("hello world!"));
+console.log(chalk.yellow.bold("hello world!"));
+console.log(chalk.red.bgYellow("hello world!"));
+console.log(chalk.yellow.underline("hello world!"));
