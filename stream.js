75a28ead8eb47a22a68097ff83fc9fbab5588af9
Reshul <53306036+resel143@users.noreply.github.com>
2021-04-22 14:44:42 +0530

    Create stream.js

---
+++ stream.js
@@ -0,0 +1,27 @@
+// creatin the server and learing Streaming 
+
+const fs = require('fs');
+const http = require('http');
+
+const server = http.createServer();
+
+server.on('request',(req, res)=>{
+    
+    // fs.readFile('input.txt',(err, data)=>{
+    //     if(err){
+    //         return console.log(err);
+    //     }
+    //     res.end(data.toString());
+    // })
+
+    // create a redable stream 
+    const rstream = fs.createReadStream('input.txt');
+    rstream.on('data',(chunkdata)=>{
+            res.write(chunkdata);
+    })
+    rstream.on('end',()=>{
+        res.end();
+    })
+})
+
+server.listen(8000, '127.0.0.1');
